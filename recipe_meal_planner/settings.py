9ae024dd30e0d7b9475007b955d211adcbbe80f3
Tim Vogt Kruitbosch <Tim.Vogt@kruitbosch.nl>
2025-10-05 23:04:16 +0200

    Fix Django settings to use SECRET_KEY from environment variables

--- recipe_meal_planner/settings.py
+++ recipe_meal_planner/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2z061+7n6%!+%qem6wtu!u99*r1_#_oa@lh#_@mrul@nmq$t%'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l2z061+7n6%!+%qem6wtu!u99*r1_#_oa@lh#_@mrul@nmq$t%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
